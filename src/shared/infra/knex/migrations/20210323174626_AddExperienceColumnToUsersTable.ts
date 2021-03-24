f4688d79a8538139f28338696dfbe24771a0de13
AndreyArthur <twekylaywer@gmail.com>
2021-03-24 08:14:15 -0300

    add experience culumn to users table

---
+++ src/shared/infra/knex/migrations/20210323174626_AddExperienceColumnToUsersTable.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    ALTER TABLE users
+    ADD COLUMN experience INT NOT NULL DEFAULT 0
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    ALTER TABLE users
+    DROP COLUMN experience
+  `);
+}
