c9ec607106e84026e7be8c0d851f2ec72a823df6
AndreyArthur <twekylaywer@gmail.com>
2021-04-03 08:29:19 -0300

    using cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import app from '@root/app';
 
 const server = express();
 
+server.use(cors());
 server.use(app);
 
 server.listen(3333);
